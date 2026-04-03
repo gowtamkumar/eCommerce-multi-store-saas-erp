1748f46afb544f9f15501a35d673d9df5b1022ac
gowtamkumar <gowtampaul0@gmail.com>
2026-04-03 17:38:02 +0600

    tenantrepository convet to injectRepostiry decoreator

--- server/src/modules/system/tenant/tenant.repository.ts
+++ server/src/modules/system/tenant/tenant.repository.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common'
-import { DataSource, Repository } from 'typeorm'
+import { Repository } from 'typeorm'
 import { TenantEntity } from './entities/tenant.entity'
+import { InjectRepository } from '@nestjs/typeorm'
 
 @Injectable()
-export class TenantRepository extends Repository<TenantEntity> {
-  constructor(private dataSource: DataSource) {
-    super(TenantEntity, dataSource.createEntityManager())
-  }
+export class TenantRepository {
+  constructor(
+    @InjectRepository(TenantEntity)
+    private readonly tenantRepo: Repository<TenantEntity>,
+  ) { }
 
   async findTenantById(id: string): Promise<TenantEntity | null> {
-    return await this.findOne({ where: { id } })
+    return await this.tenantRepo.findOne({ where: { id } })
   }
 
   async findById(id: string): Promise<TenantEntity | null> {
@@ -17,50 +19,50 @@ export class TenantRepository extends Repository<TenantEntity> {
   }
 
   async findByIdWithRelations(id: string): Promise<TenantEntity | null> {
-    return await this.findOne({
+    return await this.tenantRepo.findOne({
       where: { id },
       relations: ['subscriptionPlan'],
     })
   }
 
   async findByIdWithUser(id: string): Promise<TenantEntity | null> {
-    return await this.findOne({
+    return await this.tenantRepo.findOne({
       where: { id },
       relations: ['user'],
     })
   }
 
   async findBySubdomain(subdomain: string): Promise<TenantEntity | null> {
-    return await this.findOne({ where: { subdomain } })
+    return await this.tenantRepo.findOne({ where: { subdomain } })
   }
 
   async findByCustomDomain(customDomain: string): Promise<TenantEntity | null> {
-    return await this.findOne({ where: { customDomain } })
+    return await this.tenantRepo.findOne({ where: { customDomain } })
   }
 
   async findAllSorted(): Promise<TenantEntity[]> {
-    return await this.find({
+    return await this.tenantRepo.find({
       order: { createdAt: 'DESC' },
     })
   }
 
   async findCountByStatus(status?: string): Promise<number> {
-    if (!status) return await this.count()
-    return await this.count({
+    if (!status) return await this.tenantRepo.count()
+    return await this.tenantRepo.count({
       where: { status: status as any },
     })
   }
 
   async createAndSave(dto: any, subscriptionPlan?: any): Promise<TenantEntity> {
-    const tenant = this.create({
+    const tenant = this.tenantRepo.create({
       ...dto,
       subscriptionPlan,
     } as any) as unknown as TenantEntity
-    return await (this.save(tenant) as Promise<TenantEntity>)
+    return await (this.tenantRepo.save(tenant) as Promise<TenantEntity>)
   }
 
   async updateAndSave(tenant: TenantEntity, dto: any): Promise<TenantEntity> {
     Object.assign(tenant, dto)
-    return await this.save(tenant)
+    return await this.tenantRepo.save(tenant)
   }
 }
